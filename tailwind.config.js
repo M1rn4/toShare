f3dc9fe482191f8d1e6ca82664bd5ee0e716dfc4
Diego Bejar <diebejardelaguila@gmail.com>
2023-12-29 23:13:27 -0500

    develop: Add config custom tailwind based ux toShare

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,47 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif']
+      },
+      fontSize: {
+        'h1': '4.938rem',
+        'h2': '3.688rem',
+        'h3': '2.938rem',
+        'h4': '2.063rem',
+        'h5': '1.5rem',
+        'h6': '1.25rem',
+        'sub1': '1rem', 
+        'sub2': '0.875rem',  
+        'body1': '1.5rem', 
+        'body2': '1.25rem', 
+        'body3': '1rem', 
+        'body4': '0.875rem',
+        'button-medium': '1rem',
+        'button-small': '0.875rem',
+        'caption': '0.75rem',
+        'overline': '0.625rem',
+      },
+      fontWeight: {
+        bold: 700,
+        semibold: 600,
+        medium: 500,
+        regular: 400 
+      },
+      boxShadow: {
+        'level-1': '2px 4px 6px 0px #0000002E',
+        'level-2': '2px 4px 10px 0px #00000052',
+        'level-3': '2px 4px 12px 0px #0000007A',
+      },
+      backgroundColor: {
+        'elevation': '#434343',
+      },
+    },
+  },
+  
+  
   daisyui: {
     themes: [
       {
@@ -23,5 +64,5 @@ export default {
   },
   plugins: [
     require('daisyui'),
-  ],
+  ]
 }
