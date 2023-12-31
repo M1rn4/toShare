9003f8dfea63c05bfd1cbc55cdea572df60902ad
Diego Bejar <diebejardelaguila@gmail.com>
2023-12-30 21:52:02 -0500

    develop: Add navbar mobile and landing mobile

--- src/components/NavbarLanding.jsx
+++ src/components/NavbarLanding.jsx
@@ -7,11 +7,25 @@ import { MAX_WIDTH, STYLES_ANCHOR_NAVBAR, TEXT_ANCHOR_NAVBAR_SIZE_RESPONSIVE } f
 export default function Navbar() {
 
   return (
-    <div className={`navbar justify-between w-[1280px] h-[122px] p-10 max-w-[${MAX_WIDTH}] z-10`}>
+    <div className={`navbar justify-between w-[1280px] h-[122px] pt-10 pb-10 max-w-[${MAX_WIDTH}] z-10`}>
         <div className="">
         <img src={logo} />
       </div>
-      <div>
+
+      <div className="dropdown-end md:hidden">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-10 h-10 stroke-current text-white"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+          </div>
+          <ul tabIndex={0} className="menu  dropdown-content mt-3 z-[1] p-2 shadow bg-secondary3 rounded-[8px]  w-[65vw] boxShadow-level-1 text-body3 font-regular text-white space-y-2">
+            <li><a className='active:bg-white '>Home</a></li>
+            <li><a>About</a></li>
+            <li><a>Contact Us</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div className='hidden md:flex'>
         <ul className={`grid grid-cols-3 gap-[30px] ${TEXT_ANCHOR_NAVBAR_SIZE_RESPONSIVE}`}>
             <li className={`${STYLES_ANCHOR_NAVBAR}`}>
               Home
--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -14,40 +14,40 @@ export default function IndexPage() {
 
   return (
     <>
-    <div className="h-screen">
-      <div className="flex justify-center">       
+    <div className="min-h-screen md:h-screen ml-5 mr-5">
+      <div className="flex justify-center   z-50">       
         <Navbar />
       </div>
 
-      <div className="flex flex-col h-3/4 items-center justify-center  gap-[4rem] text-white">
-        <h2 className="text-h2 font-bold z-10">Imagine...</h2>
-        <div className="w-2/4  z-50">
-          <p className="line-clamp-4 font-regular text-body2 break-words tracking-[0.25px] leading-[30px] text-center whitespace-normal">
+      <div className="flex flex-col h-3/4 items-center justify-center gap-[4rem] text-white">
+        <h2 className="text-h2 font-bold z-1">Imagine...</h2>
+        <div className="max-w-[1000px]  z-1">
+          <p className="line-clamp-7 md:line-clamp-4 font-regular text-body2 break-words tracking-[0.25px] leading-[30px] text-center whitespace-normal">
             Welcome to our all-in-one platform designed to simplify the
             management and payments between freelancers and service clients. 
             We have created an intuitive and efficient ecosystem that offers an
             exceptional experience for both clients and independent professionals.
           </p>
         </div>
-        <div className="relative grid grid-cols-2 justify-items-center gap-[5rem]">
-          <ButtonIcon icon={<LuWallet2 size={20} />} text="Connect Wallet" className="w-max pl-4 pr-4 z-10" />
-          <ButtonIcon icon={<IoIosPlayCircle size={20} />} text="Try Demo" className={`${STYLES_BUTTON_OVERLINE} w-11/12 z-10`} />
-          <div className="absolute flex -gap-10 -top-[11rem]">
-            <div className=" circle-right rounded-full ">
+        
+        <div className="relative grid grid-cols-1 md:grid-cols-2 justify-items-center gap-[1rem] md:gap-[5rem] w-full md:w-max  md:w-4/4">
+          <ButtonIcon icon={<LuWallet2 size={20} />} text="Connect Wallet" className="w-full md:w-max  pl-4 pr-4 z-10" />
+          <ButtonIcon icon={<IoIosPlayCircle size={20} />} text="Try Demo" className={`${STYLES_BUTTON_OVERLINE} w-full md:w-11/12 z-10`} />
+
+          <div className="absolute flex -gap-10 md:-top-[11rem] w-full md:w-max">
+            <div className="w-[100vh] h-[35vh] md:w-[20vw]  md:h-[18vw]  circle-right rounded-full">
             </div>
-            <div className=" circle-center rounded-full">
+            <div className="w-[100vh] h-[35vh]  md:w-[20vw]  md:h-[18vw] circle-center rounded-full">
             </div>
-            <div className=" circle-left rounded-full">
+            <div className="w-[100vh] h-[35vh]  md:w-[20vw]  md:h-[18vw] circle-left rounded-full">
             </div>
           </div>
         </div>
       </div>
-
-
     </div>
     {
       init && (
-          <Particles className="-z-50" id="tsparticles" options={options} />
+          <Particles className="absolute -z-50" id="tsparticles" options={options} />
       )
     }
     </>
